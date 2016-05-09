b5c672fc3aa470753c01531715f1fcd1c4d9c17d
yogsgit <yogs26a@hotmail.com>
2016-05-09 12:32:35 +0100

    new solution

---
+++ WebApplication4/Controllers/RateCardController.cs
@@ -0,0 +1,33 @@
+﻿using AzureBillingAPI.Data;
+using AzureBillingAPI.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Claims;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzureBillingAPI.Web.Controllers
+{
+    public class RateCardController : Controller
+    {
+        public JsonResult GetUsageData(string subscriptionId= "ea2939ff-05e6-4818-9a45-6cb7a2902695",string organizationId = "600d2dee-f5ea-4019-b95b-4dfb587455e0",string startDate="",string endDate="")
+        {
+            // get subscription detail from the table storage
+            // make call to Rate card API
+            // return the response
+            var jsonString = AzureResourceManagerUtil.GetUsage(subscriptionId, organizationId, startDate, endDate);
+            return Json(jsonString, JsonRequestBehavior.AllowGet);
+
+        }
+
+        public JsonResult GetBillingData(string subscriptionId= "ea2939ff-05e6-4818-9a45-6cb7a2902695", string organizationId= "600d2dee-f5ea-4019-b95b-4dfb587455e0", string offerId= "MS-AZR-0063P", string currency="USD", string language="en-US", string regionInfo="IN")
+        {
+            // get subscription detail from the table storage
+            // make call to Rate card API
+            // return the response
+            var jsonString = AzureResourceManagerUtil.GetBilling(subscriptionId, organizationId, offerId, currency, language, regionInfo);
+            return Json(jsonString, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ WebApplication4/Controllers/SubscriptionController.cs
@@ -0,0 +1,43 @@
+﻿using AzureBillingAPI.Data;
+using AzureBillingAPI.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Claims;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzureBillingAPI.Web.Controllers
+{
+    public class SubscriptionController : Controller
+    {
+        public JsonResult Test()
+        {
+            var orgs = AzureResourceManagerUtil.GetUserOrganizations();
+            Dictionary<Organization, List<Subscription>> dictionary = new Dictionary<Organization, List<Subscription>>();
+            foreach (var item in orgs)
+            {
+                if (!dictionary.ContainsKey(item))
+                {
+                    var subscriptions = AzureResourceManagerUtil.GetUserSubscriptions(item.Id);
+                    dictionary.Add(item, subscriptions);
+                }
+            }
+
+            foreach (var subscriptions in dictionary.Values)
+            {
+                foreach (var subscription in subscriptions)
+                {
+                    UserSubscription userSubscription = new UserSubscription { RowKey = subscription.Id, PartitionKey = subscription.OrganizationId };
+                    userSubscription.ConnectedBy = subscription.ConnectedBy;
+                    subscription.ConnectedOn = DateTime.Now;
+
+                    var repo = new EntityRepo<UserSubscription>();
+                    repo.Insert(new List<UserSubscription> { userSubscription });
+                }
+            }
+            return Json(dictionary.ToList(), JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
